0305413df07b2a39da2922984ce5850a2bc081e4
ygjzs <1793523411@qq.com>
2021-02-02 21:35:31 +0800

    阅读一遍typescript入门教程

---
+++ simple/hello/02/02.ts
@@ -0,0 +1,12 @@
+function reverse(x: number): number;
+function reverse(x: string): string;
+function reverse(x: number | string): number | string {
+    if (typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    } else if (typeof x === 'string') {
+        return x.split('').reverse().join('');
+    }
+    return ""
+}
+
+console.log(reverse(123))
---
+++ simple/hello/03/03.ts
@@ -0,0 +1,5 @@
+let tom: [string, number];
+tom = ['Tom', 25];
+tom.push('male');
+
+console.log(tom)
